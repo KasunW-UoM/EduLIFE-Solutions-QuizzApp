35b3cfca033530b972ec12b56dfbe3fdb3641a1c
KasunW-UoM <kasun.18@itfac.mrt.ac.lk>
2022-01-16 00:45:02 +0530

    create the final quiz correct section

--- app/src/main/java/com/hit/kzonefoodapp/QuizResults.java
+++ app/src/main/java/com/hit/kzonefoodapp/QuizResults.java
@@ -1,8 +1,12 @@
 package com.hit.kzonefoodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class QuizResults extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class QuizResults extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_results);
+
+        final AppCompatButton startNewBtn = findViewById(R.id.StartNewBtn);
+        final TextView correctAnswer = findViewById(R.id.CorrectAnswers);
+        final TextView incorrectAnswers = findViewById(R.id.inCorrectAnswers);
+
+        final int getCorrectAnswers = getIntent().getIntExtra("correct",0);
+        final int getIncorrectAnswers = getIntent().getIntExtra("incorrect",0);
+
+        correctAnswer.setText(String.valueOf(getCorrectAnswers));
+        incorrectAnswers.setText(String.valueOf(getIncorrectAnswers));
+
+        startNewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(QuizResults.this,MainActivity.class));
+                finish();
+            }
+        });
+    }
+
+    public  void  onBackPressed(){
+
+        startActivity(new Intent(QuizResults.this,MainActivity.class));
+        finish();
+
     }
 }
